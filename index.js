089901b61f3dad9ba641275022e3bb9ade721ab6
NabilHG <nabilhajjoune@gmail.com>
2024-04-07 20:36:06 +0200

    improved client side

--- index.js
+++ index.js
@@ -3,39 +3,52 @@ document.getElementById("register").addEventListener("click", function(){
     let form = new FormData();
     let nick = document.getElementById("nick").value;
     let pass = document.getElementById("pass").value;
-    form.append('nick', JSON.stringify(nick));
-    form.append('pass', JSON.stringify(pass));
+    form.append('nick', nick);
+    form.append('pass', pass);
 
     fetch("http://localhost:8001/register", {
         method: "POST",
-        body: form
+        body: form,
+        mode: 'no-cors'
     })
-        .then(
-            (resp) => {
-                resp.json().then(
-                    (respJSON) => {
-                        console.log(respJSON);
-                        console.log(resp);
-                    }
-                )
-            }
-        )
+    .then((resp) => {
+        if (!resp.ok) {
+            throw new Error('Registration failed');
+        }
+        console.log(resp.json);
+        return resp.json();
+    })
+    .then((respJSON) => {
+        console.log(respJSON.message);
+        const messageElement = document.getElementById("message"); // Assuming you have an element for displaying messages
+        //messageElement.innerText = respJSON.message; // Set the message content
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        const messageElement = document.getElementById("message");
+        //messageElement.innerText = "Registration failed"; // Generic error message
+    });
 });
 
 let socket = new WebSocket("ws://localhost:8002");
 
+// Define WebSocket event handlers
+socket.onopen = function (evt) {
+  console.log("Conectat amb el servidor");
+  socket.send("Hola server");
+};
+
+socket.onmessage = function (evt) {
+  console.log("text del servidor:", evt.data);
+};
+
+socket.onclose = function (evt) {
+  console.log("El server tanca la connexio");
+};
 
 document.getElementById("login").addEventListener("click", function(){
-    console.log("login");
-    socket.onopen = function (evt) {
-    console.log("Conectat amb el servidor");
-    socket.send("Hola server");
-    }
-    socket.onmessage = function (evt) {
-    console.log("text del servidor:", evt.data);
-    }
-    socket.onclose = function (evt) {
-    console.log("El server tanca la connexio");
-    }
+  console.log("login");
+  // Connection should already be established at this point
+  // You can send additional messages or perform actions upon login
+});
 
-})
